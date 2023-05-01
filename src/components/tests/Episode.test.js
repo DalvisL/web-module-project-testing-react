29a287dfd0ac10676d884519b1c94fda94f3cdb2
Dalvis <Dalvis.liz27@gmail.com>
2023-05-01 19:24:28 -0400

    added tests to Show.test.js and Episode.test.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,11 +3,49 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
-test("renders without error", () => {});
+const exampleEpisodeData = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
+  name: "Chapter One: The Vanishing of Will Byers",
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+};
 
-test("renders the summary test passed as prop", () => {});
+test("renders without error", () => {
+    render(<Episode episode={exampleEpisodeData} />);
+});
 
-test("renders default image when image is not defined", () => {});
+test("renders the summary test passed as prop", () => {
+    const summaryEpisodeData = {...exampleEpisodeData, summary: "This is a test summary."};
+
+    render(<Episode episode={summaryEpisodeData} />);
+    
+    //get summaryelement by text
+    const summaryElement = screen.getByText(/this is a test summary./i);
+
+    expect(summaryElement).toBeInTheDocument();
+    expect(summaryElement).not.toBeEmptyDOMElement();
+    expect(summaryElement).toHaveTextContent(/this is a test summary./i);
+});
+
+test("renders default image when image is not defined", () => {
+    const noImageEpisodeData = {...exampleEpisodeData, image: null};
+
+    render(<Episode episode={noImageEpisodeData} />);
+
+    //get image element by alt text
+    const imageElement = screen.getByAltText(/https:\/\/i.ibb.co\/2FsfXqM\/stranger-things.png/i);
+    expect(imageElement).toBeInTheDocument();
+});
 
 // ----- EXAMPLE EPISODE TEST OBJECT -----
 // const exampleEpisodeData = {
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -2,13 +2,60 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event';
 
-test('renders without errors', () => { });
+const showTestObject = {
+    name: "Test Show",
+    seasons: [{
+        id: 1,
+        name: "Season 1",
+        episodes: []
+    },
+    {
+        id: 2,
+        name: "Season 2",
+        episodes: []
+    }],
+    summary: "This is a test summary.",
+};
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => { 
+    render(<Show show={showTestObject} selectedSeason={"none"}/>);
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => { 
+    render(<Show show={null} selectedSeason={"none"}/>);
+    const loadingElement = screen.getByTestId(/loading-container/i);
+    expect(loadingElement).toBeInTheDocument();
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('renders same number of options seasons are passed in', () => { 
+    render(<Show show={showTestObject} selectedSeason={"none"}/>);
+    const seasonOptions = screen.getAllByTestId(/season-option/i);
+    expect(seasonOptions).toHaveLength(2);
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('handleSelect is called when an season is selected', async () => { 
+    const handleSelect = jest.fn((event) => {
+        console.log('selected season: ', event.target.value);
+    });
+
+    render(<Show show={showTestObject} selectedSeason={"none"} handleSelect={handleSelect}/>);
+    // setting up userEvent instance to use selectOptions method
+    const user = userEvent.setup();
+    const select = screen.getByLabelText(/select a season/i);
+    await user.selectOptions(select, '1');
+    
+    expect(handleSelect).toHaveBeenCalledTimes(1); 
+    
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={showTestObject} selectedSeason={"none"}/>);
+    let episodes = screen.queryByTestId(/episodes-container/i);
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={showTestObject} selectedSeason={1}/>);
+    episodes = screen.queryByTestId(/episodes-container/i);
+    expect(episodes).toBeInTheDocument();
+ });
